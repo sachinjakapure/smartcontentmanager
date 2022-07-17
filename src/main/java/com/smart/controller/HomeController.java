33adaa49a2ec5e725f6dafc5135186a742fbfce5
Sachin <Sachinjakapure09@gmail.com>
2022-07-17 16:19:05 +0530

    first commit

---
+++ src/main/java/com/smart/controller/HomeController.java
@@ -0,0 +1,21 @@
+package com.smart.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.smart.dao.UserRepository;
+import com.smart.entities.Contacts;
+import com.smart.entities.User;
+@Controller
+public class HomeController {
+	 @RequestMapping("/home")
+	public String home(Model model) {
+		 model.addAttribute("title","Home-smart Contact Manager");
+		return "home";
+	}
+}
+
